646adb93836dfa066ab5a3302224babdf6e95e63
jezer <jezercrespo@gmail.com>
2013-03-02 19:41:18 +0800

    new records faculty subject users

--- tracker.sql
+++ tracker.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 02, 2013 at 11:06 AM
+-- Generation Time: Mar 02, 2013 at 12:39 PM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -50,11 +50,27 @@ CREATE TABLE IF NOT EXISTS `faculties` (
   `name` varchar(50) DEFAULT NULL,
   `contactno` varchar(50) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `userid` int(5) NOT NULL,
+  `userid` int(5) DEFAULT NULL,
   `createdAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `userid` (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `faculties`
+--
+
+INSERT INTO `faculties` (`id`, `name`, `contactno`, `email`, `userid`, `createdAt`) VALUES
+('1000001', 'Amparo Vallestero', '09123456789', 'amparo,vallestero@yahoo.com', NULL, NULL),
+('1000002', 'Daniel Dazig Jr', '09447890123', 'daniel.dazigjr@gmail.com', NULL, NULL),
+('1000003', 'Brandon Sibbaluca', '09234563987', 'brandon.sibbaluca@gmail.com', NULL, NULL),
+('1000004', 'Mengvi Gatpandan', '09346547890', 'menvgi.gatpandan@yahoo.com', NULL, NULL),
+('1000005', 'Fely Ordona', '09154466778', 'fely.ordona@yahoo.com', NULL, NULL),
+('1000006', 'Nelly De Leon', '09991122334', 'nelly.deleon@yahoo.com', NULL, NULL),
+('1000007', 'Jry Marie Reyes', '09216587432', 'jrymarie.reyes@gmail.com', NULL, NULL),
+('1000008', 'Mary Ann Taduyo', '09348899001', 'maryann.taduyo@yahoo.com', NULL, NULL),
+('1000009', 'Arlene Valderama', '09449092323', 'arlene.valderama@yahoo.com', NULL, NULL),
+('1000010', 'Mary Ellaine Cervantes', '09783434676', 'maryellaine.cervantes@yahoo.com', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -93,6 +109,17 @@ CREATE TABLE IF NOT EXISTS `sections` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `sections`
+--
+
+INSERT INTO `sections` (`id`) VALUES
+('301I'),
+('302I'),
+('303I'),
+('304I'),
+('305I');
+
 -- --------------------------------------------------------
 
 --
@@ -105,6 +132,19 @@ CREATE TABLE IF NOT EXISTS `subjects` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `subjectdesc`) VALUES
+('CSC 15', 'INTRO INFO TECH'),
+('ITC 12', 'IBM TRACK MOD2'),
+('ITC 16', 'DISCRETE STRUC'),
+('ITC 18', 'ADV PROGRAMING'),
+('ITC 25', 'INTRO TO PROG'),
+('ITC 33', 'OOP 1'),
+('ITC 39', 'COMP SYS ORG');
+
 -- --------------------------------------------------------
 
 --
@@ -114,11 +154,31 @@ CREATE TABLE IF NOT EXISTS `subjects` (
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) DEFAULT NULL,
-  `password` varchar(20) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
   `type` varchar(20) DEFAULT NULL,
   `createdAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `type`, `createdAt`) VALUES
+(1, 'admin', 'd033e22ae348aeb5660f', 'admin', '2013-03-02 19:29:56'),
+(2, 'operator', 'fe96dd39756ac41b7428', 'operator', '2013-03-02 19:29:56'),
+(3, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin', '2013-03-02 19:31:59'),
+(4, 'operator', 'fe96dd39756ac41b74283a9292652d366d73931f', NULL, '2013-03-02 19:31:59'),
+(5, '1000001', 'f1720daf89ee3de12e77dd69c70852839e12714f', 'faculty', '2013-03-02 19:33:24'),
+(6, '1000002', '6fb4f7fc86976a2579632bd9896e8c17e077c1ed', 'faculty', '2013-03-02 19:33:24'),
+(7, '1000003', 'b4ab3657d24f045fb9c9fdb70d635024a1be32ef', 'faculty', '2013-03-02 19:34:23'),
+(8, '1000004', 'bcafff211289a3430cfe1a02523c0dafab43102f', 'faculty', '2013-03-02 19:34:23'),
+(9, '1000005', '3fef159cafe7d3fceea70eddabe30c8398950425', 'faculty', '2013-03-02 19:35:14'),
+(10, '1000006', 'd6a4ade38549b45fb6f6a635e83ba11609812072', 'faculty', '2013-03-02 19:35:14'),
+(11, '1000007', '620afa848142cd62b8db370ff896783b91ffb609', 'faculty', '2013-03-02 19:36:18'),
+(12, '1000008', '313c565bf8eff2fc8be4906b5616ee6b3491701e', 'faculty', '2013-03-02 19:36:18'),
+(13, '1000009', 'd8bf0b292eb01f794fde6b05ee7c28033a9989b0', 'faculty', '2013-03-02 19:37:25'),
+(14, '1000010', '66e966d445ef4277e7b76fa380a786913ac4571f', 'faculty', '2013-03-02 19:37:25');
 
 --
 -- Constraints for dumped tables
