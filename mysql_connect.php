0c4b3553e327f3e8618d8aac8ce31890ee3746e6
Jude Manzano <judsDMan@yahoo.com>
2013-03-02 20:17:29 +0800

    Added database connecton file

---
+++ mysql_connect.php
@@ -0,0 +1,11 @@
+<html>
+<?php
+
+$username="root";
+$password="";
+$database="tracker";
+
+mysql_connect(localhost, $username, $password);
+@mysql_select_db($database) or die ("Unable to select database!");
+?>
+</html>
