7c8816d9b2e732ab69e3ba67e50db31aa568abb9
jezer <jezercrespo@gmail.com>
2013-03-02 17:56:45 +0800

    created template for admin

---
+++ admin/index.php
@@ -0,0 +1,21 @@
+        <?php include("header.php");?>
+        
+                <div class="span4">
+               CONTENT HERE
+               asd
+               as
+               das
+               das
+               d
+               asd
+               as
+               das
+               das
+               d
+               asd
+               
+                   
+            	</div>
+
+        <!-- /container -->
+     <?php include("footer.php");?>
