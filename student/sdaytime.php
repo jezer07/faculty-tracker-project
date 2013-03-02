112bb4275d39b26b128f55624688d12f9b468c2e
jezer <jezercrespo@gmail.com>
2013-03-02 19:26:54 +0800

    added dynamic active class to the sub nav on student

---
+++ student/sdaytime.php
@@ -0,0 +1,65 @@
+        
+		
+		<?php include("header.php");?>
+        
+            
+             
+                <div class="span9">
+          		
+                
+                <h3>Search by Name</h3><form class="form-search">
+    <input type="text" class="input-medium search-query">
+    <button type="submit" class="btn">Search</button>
+    </form>
+                <hr/>
+           <table class="table table-striped">
+           	<caption>Marry Adnn Taduyo<caption>
+            <thead>
+           		<tr>
+            		<th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    	
+           	   </tr>
+            </thead>
+            <tbody>
+            	<tr>
+                	<td>ITC30-304i<br/>7:30-8:30<br/>H-410</td>
+                    <td>a</td>
+                    <td>a</td>
+                    <td>a</td>
+                    <td>a</td>
+                </tr>
+                
+                 	<tr>
+                	<td>ITC30-304i<br/>7:30-8:30<br/>H-410</td>
+                    <td>a</td>
+                    <td>a</td>
+                    <td>a</td>
+                    <td>a</td>
+                </tr>
+            
+            </tbody>
+            
+           
+           </table>     
+           
+          			
+                    
+                    
+                    
+                    
+                   
+            	</div>
+
+        <!-- /container -->
+     <?php include("footer.php");?>
+	  <script>
+            $(document).ready(function(){
+				$("#sdaytime").attr("class","active");
+			
+				});
+             
+             </script>
---
+++ student/sname.php
@@ -0,0 +1,58 @@
+        <?php include("header.php");?>
+        
+             <script>
+            
+             
+             </script>
+             
+                <div class="span6">
+          		
+                
+                <h3>Search by Name</h3><form class="form-search">
+    <input type="text" class="input-medium search-query">
+    <button type="submit" class="btn">Search</button>
+    </form>
+                <hr/>
+           <table class="table table-striped">
+           	<caption>Marry Adnn Taduyo<caption>
+            <thead>
+           		<tr>
+            		<th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    	
+           	   </tr>
+            </thead>
+            <tbody>
+            	<tr>
+                	<td>ITC30-304i<br/>7:30-8:30<br/>H-410></td>
+                    <td>a</td>
+                    <td>a</td>
+                    <td>a</td>
+                    <td>a</td>
+                </tr>
+            
+            </tbody>
+            
+           
+           </table>     
+           
+          			
+                    
+                    
+                    
+                    
+                   
+            	</div>
+
+        <!-- /container -->
+     <?php include("footer.php");?>
+	   <script>
+            $(document).ready(function(){
+				$("#sdaytime").attr("class","active");
+			
+				});
+             
+             </script>
